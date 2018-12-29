c6b39af36a917076c13546cd5a22485a072f9310
Robin Yuen Shing Hei <robin.ysh@gmail.com>
2018-12-30 02:31:59 +0800

    Baseline CNN Model

--- model.py
+++ model.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 class Network(nn.Module):
     def __init__(self):
         super(Network, self).__init__()
-        #self.conv1 = nn.Conv2d(1, 20, 3, 1)
-        #self.conv2 = nn.Conv2d(20, 40, 3, 1)
-        self.fc1 = nn.Linear(784, 40)
+        self.conv1 = nn.Conv2d(1, 20, 3, 1)
+        self.conv2 = nn.Conv2d(20, 40, 3, 1)
+        self.fc1 = nn.Linear(1000, 40)
         self.fc2 = nn.Linear(40, 10)
 
         for m in self.modules():
@@ -18,10 +18,10 @@ class Network(nn.Module):
 
 
     def forward(self, x):
-        #x = F.relu(self.conv1(x))
-        #x = F.max_pool2d(x, 2)
-        #x = F.relu(self.conv2(x))
-        #x = F.max_pool2d(x, 2)
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2)
         x = x.view(x.size(0), -1)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
